85843da456ab031d9e07d9ceeea8fb6f6a317297
ydr29913 <ydr29913@educastur.es>
2025-02-18 12:46:05 +0100

    Creacion del proyecto y dependencias

---
+++ tarea7dwesYDR29913/src/main/java/com/ydr29913/tarea7dwesYDR29913/Tarea7dwesYdr29913Application.java
@@ -0,0 +1,13 @@
+package com.ydr29913.tarea7dwesYDR29913;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea7dwesYdr29913Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea7dwesYdr29913Application.class, args);
+	}
+
+}
---
+++ tarea7dwesYDR29913/src/test/java/com/ydr29913/tarea7dwesYDR29913/Tarea7dwesYdr29913ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydr29913.tarea7dwesYDR29913;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea7dwesYdr29913ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
